99718b48e0a62621607ba250675fcc5d62902b7a
furkandadali <alifdadali@gmail.com>
2022-03-31 00:02:10 +0300

    api and orders services path changed.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApiService } from './api/api.service';
+import { OrdersService } from './orders/orders.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ApiService, OrdersService],
 })
 export class AppModule {}
